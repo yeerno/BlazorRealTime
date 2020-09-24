d8807236d3bc1912faa3133114380d7e17bec89d
Marcin Ziętek <mzietek@mmm.com>
2020-09-24 11:35:40 +0200

    Add project files.

---
+++ BlazorRealTime.Server/NotificationHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRealTime.Server
+{
+    public class NotificationHub : Hub
+    {
+        public NotificationHub()
+        {
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+    }
+}
---
+++ BlazorRealTime.Server/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorRealTime.Server
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Added AddCors "CorsPolicy"
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            services.AddControllers();
+
+            //SignalR added
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //Added MApHub with path request "/notificationhub"
+                endpoints.MapHub<NotificationHub>("/notificationhub");
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
